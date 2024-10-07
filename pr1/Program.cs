aef8b197eb053de7fcab7ddda973d92e9c584cf3
ShchokinYuriy <st7363087@stud.duikt.ua>
2024-10-07 22:36:46 +0300

    Released class1

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace pr1
 {
-    class NumbersOfDiapasone{}
+    class NumbersOfDiapasone{
+        private int[] arr;
+        private int n;
+        private Random random = new Random();
+
+
+        public NumbersOfDiapasone(int[] arr, int n)
+        {
+            this.arr = arr;
+            this.n = n;
+        }
+
+
+        public void RandomArr()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 100);
+            }
+        }
+
+        public void CheckDiapasone()
+        {
+            foreach (int value in arr)
+            {
+                if (value > 0 && value < n)
+                {
+                    Console.WriteLine(value + "true");
+                }
+                else
+                {
+                    Console.WriteLine(value + "False");
+                }
+            }
+        }
+    }
 
     class Tringle{}
 
