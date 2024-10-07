10b6bff6cb0f047f2931b46d00adfa4d65b4e679
ShchokinYuriy <st7363087@stud.duikt.ua>
2024-10-07 22:52:17 +0300

    Released class2

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -8,7 +8,61 @@ namespace pr1
 {
     class NumbersOfDiapasone{}
 
-    class Tringle{}
+    class Tringle{
+        private int[] arr;//private array
+
+        public Tringle(int[] arr)//class constructor
+        {
+            this.arr = arr;
+
+        }
+
+        public void CheackTringle()
+        {
+            if (arr[0] + arr[1] > arr[2] && arr[0] + arr[2] > arr[1] && arr[1] + arr[2] > arr[0])
+            {
+                Console.WriteLine("Tringle is tringle :)");
+            }
+            else
+            {
+                Console.WriteLine("Tringle is not tringle :(");
+            }
+        }
+        public void TringleP()//P tringle
+        {
+            if (arr[0] + arr[1] > arr[2] && arr[0] + arr[2] > arr[1] && arr[1] + arr[2] > arr[0])
+            {
+                float P = 0;
+                foreach (int i in arr)
+                {
+                    P += i;
+                }
+                Console.WriteLine("P=" + P);
+            }
+            else
+            {
+                Console.WriteLine("ERROR!!!ERROR!!!ERROR!!!ERROR!!!ERROR!!!ERROR!!!");
+            }
+        }
+        public void TringleS()//S tringle
+        {
+            if (arr[0] + arr[1] > arr[2] && arr[0] + arr[2] > arr[1] && arr[1] + arr[2] > arr[0])
+            {
+                float S = 0;
+                foreach (int i in arr)
+                {
+                    S += i;
+                }
+                S /= 2;
+                S = (float)Math.Sqrt(S * (S - arr[0]) * (S - arr[1]) * (S - arr[2]));
+                Console.WriteLine("S=" + S);
+            }
+            else
+            {
+                Console.WriteLine("ERROR!!!ERROR!!!ERROR!!!ERROR!!!ERROR!!!ERROR!!!");
+            }
+        }
+    }
 
     class BigArray{}
 
