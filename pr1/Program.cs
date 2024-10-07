4e92263105d1db0b5b1286a1925682e353d1e1a5
ShchokinYuriy <st7363087@stud.duikt.ua>
2024-10-07 22:56:28 +0300

    Released class3

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -12,7 +12,62 @@ namespace pr1
     
     }
 
-    class BigArray{}
+    class BigArray{
+        private int[] arr;//private array
+        private Random random = new Random();
+
+        public BigArray(int[] arr)//class constructor
+        {
+            this.arr = arr;
+        }
+
+        public void GenerateArray()
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(0, arr.Length);
+
+            }
+        }
+        public void ShowArray()
+        {
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void ShowMin()
+        {
+            int min = 100;
+            foreach (int i in arr)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+
+            Console.WriteLine("minimal number = " + min);
+        }
+        public void ShowMax()
+        {
+            int max = 0;
+            foreach (int i in arr)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+
+            Console.WriteLine("maximal number = " + max);
+        }
+
+
+
+
+    }
 
     class ArrayFilter{}
 
